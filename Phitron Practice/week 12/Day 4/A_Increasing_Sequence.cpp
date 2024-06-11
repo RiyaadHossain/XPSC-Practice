dcb9476cce28240f9f438f52150814fdbf6be56d
RiyaadHossain <riyadhossain.dev@gmail.com>
2024-06-11 16:54:12 +0600

    week - 12; Day - 4

---
+++ Phitron Practice/week 12/Day 4/A_Increasing_Sequence.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define endl '\n'
+#define ye cout << "YES"
+#define no cout << "NO"
+#define int long long
+#define print(...) cout << (__VA_ARGS__)
+#define println(...) cout << (__VA_ARGS__) << '\n'
+
+// Shortcut functions
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define a first
+#define b second
+
+// Data Type
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+// Constant Varialbes
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+const int llinf = 1e18;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    vi b(n);
+    b[0] = arr[0] == 1 ? 2 : 1;
+    for (int i = 1; i < n; i++)
+    {
+        if(b[i-1]+1 != arr[i])
+            b[i] = b[i - 1] + 1;
+        else 
+            b[i] = b[i - 1] + 2;
+    }
+
+    print(b.back());
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+        cout << '\n';
+    }
+
+    return 0;
+}
