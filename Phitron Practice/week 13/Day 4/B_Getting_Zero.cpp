0c7a03776edf4909120dd508a9d6807efebc4877
RiyaadHossain <riyadhossain.dev@gmail.com>
2024-06-25 16:57:15 +0600

    week - 13; Day - 4

---
+++ Phitron Practice/week 13/Day 4/B_Getting_Zero.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define endl '\n'
+#define ye cout << "YES"
+#define no cout << "NO"
+#define int long long
+#define print(...) cout << (__VA_ARGS__)
+#define printsp(...) cout << (__VA_ARGS__) << ' '
+#define println(...) cout << (__VA_ARGS__) << '\n'
+
+// Shortcut functions
+#define a first
+#define b second
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define countSetBit(x) __builtin_popcount(x)
+
+// Data Type
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+// Constant Varialbes
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+const int llinf = 1e18;
+const int N = 32768;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int ans = 15;
+    for (int i = 0; i <= 15; i++)
+    {
+        for (int j = 0; j <= 15; j++)
+        {
+            if (((n + i) << j) % N == 0)
+                ans = min(ans, i + j);
+        }
+    }
+
+    printsp(ans);
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
---
+++ Phitron Practice/week 13/Day 4/C_Deep_Down_Below.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define endl '\n'
+#define ye cout << "YES"
+#define no cout << "NO"
+#define int long long
+#define print(...) cout << (__VA_ARGS__)
+#define printsp(...) cout << (__VA_ARGS__) << ' '
+#define println(...) cout << (__VA_ARGS__) << '\n'
+
+// Shortcut functions
+#define a first
+#define b second
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define countSetBit(x) __builtin_popcount(x)
+
+// Data Type
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+// Constant Varialbes
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+const int llinf = 1e18;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    map<pii, pii> mpp;
+    for (int i = 0; i < n; i++)
+    {
+        int mx = 0, k;
+        cin >> k;
+        vi cave(k);
+
+        for (int j = 0; j < k; j++)
+        {
+            cin >> cave[j];
+            mx = max(mx, cave[j] - j);
+        }
+
+        mpp[{mx, i}] = {mx, k};
+    }
+
+    int power = ((*mpp.begin()).a.a) + 1;
+    int init = power;
+    for (auto [x, cave] : mpp)
+    {
+        int armor = cave.a, k = cave.b;
+        if (power <= armor)
+            init += (armor + 1 - power), power = armor + 1;
+        power+=k;
+    }
+
+    print(init);
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+        cout << '\n';
+    }
+
+    return 0;
+}
