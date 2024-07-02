16d4cab4162a93da661159567bb0726868c69de8
RiyaadHossain <riyadhossain.dev@gmail.com>
2024-07-02 16:57:59 +0600

    week - 14; Day - 4

---
+++ Phitron Practice/week 14/Day 4/E_Queue_Sort.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define endl '\n'
+#define ye cout << "YES"
+#define no cout << "NO"
+#define int long long
+#define print(...) cout << (__VA_ARGS__)
+#define printsp(...) cout << (__VA_ARGS__) << ' '
+#define println(...) cout << (__VA_ARGS__) << '\n'
+
+// Shortcut functions
+#define a first
+#define b second
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define countSetBit(x) __builtin_popcount(x)
+#define dsFind(ds, x) ds.find(x) != ds.end()
+
+// Data Type
+typedef vector<int> vi;
+typedef vector<vi> vii;
+typedef pair<int, int> pi;
+typedef map<int, int> mi;
+
+// Constant Varialbes
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+const int llinf = 1e18;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vi a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    vi pre(n), suff(n);
+    pre[0] = a[0];
+    for (int i = 1; i < n; i++)
+        pre[i] = min(pre[i - 1], a[i]);
+
+    suff[n - 1] = a[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+        suff[i] = min(suff[i + 1], a[i]);
+
+    bool isPoss = 1;
+    for (int i = 1; i < n - 1; i++)
+    {
+        if (a[i] > suff[i] && pre[i - 1] <= suff[i + 1])
+            isPoss = 0;
+    }
+
+    if (!isPoss)
+    {
+        print(-1);
+        return;
+    }
+
+    auto notSame = [&](int id) -> bool
+    {
+        if (id > 0)
+            return a[id] != a[id - 1];
+        return true;
+    };
+
+    int idx = 0;
+    for (int i = 0; i < n; i++)
+        if (suff[0] == a[i] && notSame(i))
+            idx = i;
+
+    if (is_sorted(all(a)))
+        idx = 0;
+
+    print(idx);
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+        cout << '\n';
+    }
+
+    return 0;
+}
