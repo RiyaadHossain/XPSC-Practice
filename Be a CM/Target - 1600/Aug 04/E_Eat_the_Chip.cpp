e4fc99410dcb140f15ddcb95c72b1ce5362dc247
RiyaadHossain <riyadhossain.dev@gmail.com>
2024-08-04 16:49:29 +0600

    Target 1600; Aug - 04

---
+++ Be a CM/Target - 1600/Aug 04/E_Eat_the_Chip.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// Utilities
+#define endl '\n'
+#define int long long
+#define no cout << "NO"
+#define ye cout << "YES"
+#define print(...) cout << (__VA_ARGS__)
+#define printsp(...) cout << (__VA_ARGS__) << ' '
+#define println(...) cout << (__VA_ARGS__) << '\n'
+
+// Looping Mechanism
+#define forEach(x, ds) for (auto &x : ds)
+#define forr(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rforr(i, a, b) for (int i = (a); i >= (b); --i)
+
+// Shortcut Functions
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define countSetBit(x) __builtin_popcountll(x)
+#define dsFind(x, ds) (ds.find(x) != ds.end())
+
+// Data Types
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef map<int, int> mi;
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+
+// Constant Varialbes
+const int inf = 1e9;
+const int llinf = 1e18;
+const int mod = 1e9 + 7;
+
+string d = "Draw";
+string a = "Alice";
+string b = "Bob";
+
+void solve()
+{
+    int row, col, a1, a2, b1, b2;
+    cin >> row >> col >> a1 >> a2 >> b1 >> b2;
+
+    if (a1 >= b1)
+    {
+        print(d);
+        return;
+    }
+
+    int rowDis = abs(a1 - b1) - 1;
+    int colDis = abs(a2 - b2);
+
+    if (rowDis % 2 == 0)
+    {
+        if (colDis <= 1)
+            print(a);
+        else if (a2 < b2)
+            col <= a2 + (rowDis / 2 + 1) ? print(a) : print(d);
+        else
+            1 >= a2 - (rowDis / 2 + 1) ? print(a) : print(d);
+    }
+    else
+    {
+        if (a2 == b2)
+            print(b);
+        else if (a2 < b2)
+            1 >= b2 - (rowDis / 2 + 1) ? print(b) : print(d);
+        else
+            col <= b2 + (rowDis / 2 + 1) ? print(b) : print(d);
+    }
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+        cout << '\n';
+    }
+
+    return 0;
+}
