05887d499381aea4c99cf3a9bfd69a7dd1edf2ae
RiyaadHossain <riyadhossain.dev@gmail.com>
2024-08-12 16:41:19 +0600

    Week - 16; Day - 03

---
+++ Phitron Practice/week 17/Day 3/C_Choose_the_Different_Ones.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// Utilities
+#define endl '\n'
+#define int long long
+#define no cout << "NO"
+#define ye cout << "YES"
+#define print(...) cout << (__VA_ARGS__)
+#define printsp(...) cout << (__VA_ARGS__) << ' '
+#define println(...) cout << (__VA_ARGS__) << '\n'
+
+// Looping Mechanism
+#define forEach(x, ds) for (auto &x : ds)
+#define forr(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rforr(i, a, b) for (int i = (a); i >= (b); --i)
+
+// Shortcut Functions
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define countSetBit(x) __builtin_popcountll(x)
+#define dsFind(x, ds) (ds.find(x) != ds.end())
+
+// Data Types
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef map<int, int> mi;
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+
+// Constant Varialbes
+const int inf = 1e9;
+const int llinf = 1e18;
+const int mod = 1e9 + 7;
+
+void solve()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    vi a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    vi b(m);
+    for (int i = 0; i < m; i++)
+        cin >> b[i];
+
+    set<int> st1, st2;
+    forr(i, 0, n - 1)
+    {
+        if (a[i] <= k)
+            st1.insert(a[i]);
+    }
+
+    forr(i, 0, m - 1)
+    {
+        if (b[i] <= k)
+            st2.insert(b[i]);
+    }
+
+    set<int> st;
+    st.insert(all(st1));
+    st.insert(all(st2));
+
+    if (sz(st1) < k / 2 || sz(st2) < k / 2)
+        no;
+    else
+        sz(st) == k ? ye : no;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+        cout << '\n';
+    }
+
+    return 0;
+}
---
+++ Phitron Practice/week 17/Day 3/F_Forever_Winter.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// Utilities
+#define endl '\n'
+#define int long long
+#define no cout << "NO"
+#define ye cout << "YES"
+#define print(...) cout << (__VA_ARGS__)
+#define printsp(...) cout << (__VA_ARGS__) << ' '
+#define println(...) cout << (__VA_ARGS__) << '\n'
+
+// Looping Mechanism
+#define forEach(x, ds) for (auto &x : ds)
+#define forr(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rforr(i, a, b) for (int i = (a); i >= (b); --i)
+
+// Shortcut Functions
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define countSetBit(x) __builtin_popcountll(x)
+#define dsFind(x, ds) (ds.find(x) != ds.end())
+
+// Data Types
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef map<int, int> mi;
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+
+// Constant Varialbes
+const int inf = 1e9;
+const int llinf = 1e18;
+const int mod = 1e9 + 7;
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+
+    map<int, vi> mpp;
+    while (m--)
+    {
+        int u, v;
+        cin >> u >> v;
+        mpp[u].pb(v);
+        mpp[v].pb(u);
+    }
+
+    vi nodes;
+    for (auto [x, y] : mpp)
+        if (sz(y) >= 2)
+            nodes.pb(sz(y));
+
+    sort(all(nodes));
+    if (nodes[0] == nodes[1])
+        cout << nodes.back() << " " << nodes.front() - 1;
+    else
+        cout << nodes.front() << " " << nodes.back() - 1;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+        cout << '\n';
+    }
+
+    return 0;
+}
