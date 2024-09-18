0ea77ef271828353dccaea80828bf3cfc3d2574f
RiyaadHossain <riyadhossain.dev@gmail.com>
2024-09-18 18:11:16 +0600

    Week - 21; Day - 2

---
+++ Phitron Practice/week 21/Day 2/A_Setting_up_Camp.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// Utilities
+#define endl '\n'
+#define int long long
+#define no cout << "NO"
+#define ye cout << "YES"
+#define print(...) cout << (__VA_ARGS__)
+#define printsp(...) cout << (__VA_ARGS__) << ' '
+#define println(...) cout << (__VA_ARGS__) << '\n'
+
+// Looping Mechanism
+#define forEach(x, ds) for (auto &x : ds)
+#define forr(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rforr(i, a, b) for (int i = (a); i >= (b); --i)
+
+// Shortcut Functions
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define countSetBit(x) __builtin_popcountll(x)
+#define dsFind(x, ds) (ds.find(x) != ds.end())
+#define lb(x, ds) lower_bound(ds.begin(), ds.end(), x)
+#define ub(x, ds) upper_bound(ds.begin(), ds.end(), x)
+
+// Data Types
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef map<int, int> mi;
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+
+// Constant Varialbes
+const int inf = 1e9;
+const int llinf = 1e18;
+const int mod = 1e9 + 7;
+
+void solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int ans = a;
+    ans += (b + 2) / 3;
+    b %= 3;
+    c -= b == 0 ? 0 : (3 - b);
+
+    if (c < 0)
+    {
+        print(-1);
+        return;
+    }
+
+    ans += (c + 2) / 3;
+    print(ans);
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+        cout << endl;
+    }
+
+    return 0;
+}
